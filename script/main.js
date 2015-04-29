ebfcd0fb6bab341ed879a57248d41142c1967d6f
rs222kn <richardsoderman@richards-MacBook-Air.local>
2015-04-29 17:33:29 +0200

    V1.0

--- script/main.js
+++ script/main.js
@@ -1,4 +1,34 @@
+'use strict';
 
+function getMouseCoords(e) {
+  var e = e || window.event;
+  //document.getElementById('msg').innerHTML = e.clientX + ', ' + 
+   //        e.clientY + '<br>' + e.screenX + ', ' + e.screenY;
+}
+
+
+var followCursor = (function() {
+  var s = document.createElement('div');
+  s.setAttribute('class', 'folow');
+
+  return {
+    init: function() {
+      document.body.appendChild(s);
+    },
+
+    run: function(e) {
+      var e = e || window.event;
+      s.style.left  = (e.clientX - 20) + 'px';
+      s.style.top = (e.clientY - 50) + 'px';
+      getMouseCoords(e);
+    }
+  };
+}());
+
+window.onload = function() {
+  //followCursor.init();
+  //document.body.onmousemove = followCursor.run;
+}
 
 
 
